44dad5492106a1255c0f5b1e3b8f835f03552779
xiaodabao <xiaojin1992@126.com>
2019-07-27 20:25:07 +0800

    fs.proimses is expermential

--- src/recursive-readdir.ts
+++ src/recursive-readdir.ts
@@ -1,11 +1,13 @@
-import { promises as fsPromises } from 'fs';
+import * as fs from 'fs';
+import * as util from 'util';
 import * as path  from 'path';
 
+const readdirPromise = util.promisify(fs.readdir);
 
 export default async (directory: string) => {
     const files: string[] = [];
     async function loop(dir: string) {
-        const subItems = await fsPromises.readdir(dir, { withFileTypes: true });
+        const subItems = await readdirPromise(dir, { withFileTypes: true });
         for (const item of subItems) {
             if (item.isDirectory()) {
                 await loop(path.join(dir, item.name));
